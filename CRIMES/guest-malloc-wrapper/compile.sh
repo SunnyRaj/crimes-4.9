25a6f76a5edf9a57bbf4fbbde9267d7c7cef3e06
Zhen Ni <leonizhen@gwu.edu>
2017-10-10 11:31:03 -0400

    fix typo in event monitoring script

--- CRIMES/guest-malloc-wrapper/compile.sh
+++ CRIMES/guest-malloc-wrapper/compile.sh
@@ -1,3 +1,3 @@
 gcc -o test_malloc test_malloc.c
-gcc -o test-single-memevent test-single-memevent.c
+gcc -o test-single-memevent test_single_memevent.c
 gcc -O2 -Wall -Werror -fPIC -o prot_malloc.so -shared prot_malloc.c -I.
